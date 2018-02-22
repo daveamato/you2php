2c33ee00c8ffc9be550486cbb0124c374b0d4cab
David Amato <daveamato@live.com>
2018-02-22 02:02:08 -0500

    bump

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define('ROOT_PART', Root_part());
+define('APIKEY', 'AIzaSyAtVoFXulg-stpH878Sbs_OWrKdTOeE5bE');
+define('GJ_CODE', 'US');
+define('SITE_NAME', 'ChowderTube');
+define('TITLENAME', 'ChowderTube');
+define('EN2DEKEY', 'eQ7GpOWuDMpeSWOSj7aWrFpl2xTyNLfN');
+define('EMAIL', 'chowder@amato.tk');
+?>
