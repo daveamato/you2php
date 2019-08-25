f923924a33746f9b3417a57ec0e91dabf1e35270
Dave Amato <daveamato@live.com>
2019-08-25 22:25:46 +0000

    removed 1080p

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define('ROOT_PART', Root_part());
+define('APIKEY', 'AIzaSyAtVoFXulg-stpH878Sbs_OWrKdTOeE5bE');
+define('GJ_CODE', 'US');
+define('SITE_NAME', 'ChowderTube');
+define('TITLENAME', 'ChowderTube');
+define('EN2DEKEY', 'FPBM7DvTzLL7biWdt1mxBLqeAMuPRAHc');
+define('EMAIL', 'mrlaugh01@gmail.com');
+?>
