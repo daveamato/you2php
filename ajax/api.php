e9123ca5879be0a3c53c0547f2a0d8c8857680ee
David Amato <daveamato@live.com>
2020-02-08 12:56:42 -0500

    up

--- ajax/api.php
+++ ajax/api.php
@@ -1,8 +1,8 @@
 <?php
-    include("./lib.php");
+    //include("./lib.php");
     if(!is_array($_GET)&&count($_GET)<=0){exit();}
     include('../lib.php');
-    include_once('./YouTubeDownloader.php');
+    include_once('../YouTubeDownloader.php');
     if($_GET['type'] == 'info'){
       echo json_encode(get_video_info($_GET['v'],APIKEY));
     }elseif ($_GET['type'] == 'downlink') {
