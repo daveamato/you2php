8e064d61ab7c77fa13c0bff49aa5ae2a9f9f91d9
David Amato <daveamato@live.com>
2020-07-26 18:20:24 -0400

    enables error logging

--- ajax/api.php
+++ ajax/api.php
@@ -1,4 +1,6 @@
 <?php
+    ini_set('display_errors', '1');
+    ini_set('error_log', dirname(__FILE__) . '/error_log.txt');
     if(!is_array($_GET)&&count($_GET)<=0){exit();}
     include('../lib.php');
     require('./vendor/autoload.php');
